c935596c19c098417b1f3ae3690a6857380e1e21
Ning78 <46696631+Ning78@users.noreply.github.com>
2019-01-31 22:58:32 -0500

    Update cards.rb

--- Project2/cards.rb
+++ Project2/cards.rb
@@ -27,9 +27,9 @@ end
 # Get 81 cards and put them in the array. Each of them has different number, color, shape and shade.
 def getCards
   numbers = [1,2,3];
-  colors = [diamond,squiggle, oval];
-  shapes = [solid, striped, open];
-  shades = [red, green, purple]
+  colors = [red, green, purple]
+  shapes = [diamond,squiggle, oval]
+  shades = [solid, striped, open];
   cards = Array.new(81);
   for number in numbers
     for color in colors
