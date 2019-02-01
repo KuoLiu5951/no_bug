72928e417d979e04cb7c85337a157fd5b7b772b2
Ning78 <46696631+Ning78@users.noreply.github.com>
2019-02-01 00:41:22 -0500

    Add files via upload

---
+++ Project2/cards1.rb
@@ -0,0 +1,116 @@
+class Cards1
+  def initialize(number,color,shape,shade)
+    @number = number;
+    @color = color;
+    @shape = shape;
+    @shade = shade;
+  end
+
+  def getNum
+    return @number;
+  end
+
+  def getColor
+    return @color;
+  end
+
+  def getShape
+    return @shape;
+  end
+
+  def getShade
+    return @shade;
+  end
+
+
+end
+# Get 81 cards and put them in the array. Each of them has different number, color, shape and shade.
+def getCards1
+  numbers = [1,2,3,4];
+  colors = [red, green, purple,blue]
+  shapes = [diamond,squiggle, oval,square]
+  shades = [solid, striped, empty,cross];
+  cards = Array.new(256);
+  for number in numbers
+    for color in colors
+      for shape in shapes
+        for shade in shades
+          card = Cards.initialize(number,color,shape,shade);
+          cards.add card;
+        end
+      end
+    end
+  end
+  return cards;
+end
+
+#Get random 16 cards and put them in the array. Then remove them from the original array.
+def getRandomCards1(cards)
+  i = 0;
+  cardShow = Array.new()
+  while i < 16
+    card = cards[rand(cards.length)];
+    cards.delete(card);
+    cardShow.add(card);
+    i = i + 1;
+  end
+  return cardShow;
+end
+
+#Add 4 cards to the array of cards that will be shown to the player. And remove them from the original card array.
+def addCards1(cards,cardShow)
+  i = 0;
+  while i < 4
+    card = cards[rand(cards.length)];
+    cards.delete(card);
+    cardShow.add(card);
+    i = i + 1;
+  end
+end
+
+
+#Remove card from the original array of cards
+def removeCard1(cards,cardsFormASet)
+  i = 0
+  while i < cardsFormASet.length
+    cards.delete(cardsFormASet[i]);
+    i = i + 1;
+  end
+end
+
+# Algorithm to determine 3 cards is a set or not
+def isSet1(cardChosen)
+  cardA = cardChosen[0];
+  cardB = cardChosen[1];
+  cardC = cardChosen[2];
+
+  set = false
+  if (((cardA.number == cardB.number) && (cardB.number == cardC.number) ||
+      (cardA.number != cardB.number) && (cardA.number != cardC.number) && (cardB.number != cardC.number)))
+    if (((cardA.shape== cardB.shape) && (cardB.shape== cardC.shape) ||
+        (cardA.shape != cardB.shape) && (cardA.shape != cardC.shape) && (cardB.shape != cardC.shape)))
+      if (((cardA.shade == cardB.shade) && (cardB.shade == cardC.shade) ||
+          (cardA.shade != cardB.shade) && (cardA.shade != cardC.shade) && (cardB.shade != cardC.shade)))
+        if (((cardA.color == cardB.color) && (cardB.color == cardC.color) ||
+            (cardA.color != cardB.color) && (cardA.color != cardC.color) && (cardB.color != cardC.color)))
+          set = true
+        end
+      end
+    end
+  end
+  return set
+end
+
+# If Set on the board: highlights the next card in the Set. If no Set on board: adds three new cards
+def hint1 (cardA, cardB, cardShow,cards)
+  i = 0
+  while i<cardShow.length
+    cardC = cardShow[i]
+    cardChosen = [cardA,cardB,cardC]
+    if isSet(cardChosen)
+      return cardC
+    end
+    i += 1
+  end
+  addCards(cards,cardShow)
+end
