e87360655e0ba664150b78ca805e500712357d19
Ning78 <46696631+Ning78@users.noreply.github.com>
2019-02-02 15:38:23 -0500

    Update score.rb

--- Project2/score.rb
+++ Project2/score.rb
@@ -1,26 +1,11 @@
 class Score
-  def initialize
-    @scores = 0
-    @start_time = Time.now
-    @stop_time = Time.now
-    @sum_time = 0
+  
+  #If player find a set, then get one point
+  def Add_score(cardChosen)
+    if isSet(cardChosen)
+    @score += 1
+    end
+    return @score
   end
-   #calculate the total time of playing
-    def total_time
-      @sum_time = (Time.now - @start_time).round(2)
-    end
-    #calculate time of finding a set
-     def set_time
-       @set_time = (Time.now - @stop_time).round(2)
-       @stop_time = Time.now
-       @set_time
-     end
-    #If player find a set, then get one point
-    def increment_score
-      @score += 1
-    end
-
-
-
-
+  
 end
