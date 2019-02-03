fc31f1411374562a65ba117a8cd9b05500bfb4b6
Jian Gao <1304637796@qq.com>
2019-02-02 20:22:30 -0500

    Update cards.rb

--- Project2/cards.rb
+++ Project2/cards.rb
@@ -1,30 +1,33 @@
 class Cards
-  def initialize(number, color, shape, shade, address, c_address)
-    #0 is single, 1 is double, 2 is triple
+  def initialize(number, color, shape, shade, state)
+    #1 is single, 2 is double, 3 is triple
     @number = number
 
-    # 0 is red, 1 is green, 2 is purple
+    #1 is red, 2 is green, 3 is purple
     @color = color
 
-    #0 is diamond, 1 is squiggle, 2 is oval
+    #1 is diamond, 2 is squiggle, 3 is oval
     @shape = shape
 
-    # 0 is solid, 1 is striped, 2 is  empty
+    #1 is solid, 2 is striped, 3 is  empty
     @shade = shade
-
-    # the address of images
-    @address = address
-
-    #the address of images that are clicked by users
-    @c_address = c_address
+    
+    #Chosen means this card is chosen by the user. UnChosen means this card is not chosen by the user.
+    @state = state
   end
   
-  def getAddress
-    @address
+  #Get the state of the card.
+  def getState
+    return @state
   end
-  
+
+  #Change the state of the card.
   def switch
-    @address, @c_address = @c_address, @address
+    if @state = true
+      @state  = false
+    else
+      @state  = true
+    end  
   end
 
   def getNum
@@ -46,15 +49,15 @@ class Cards
   # Get 81 cards and put them in the array. Each of them has different number, color, shape and shade.
   def getCards
     numbers = [1,2,3];
-    colors = [red, green, purple];
-    shapes = [diamond,squiggle, oval];
-    shades = [solid, striped, empty];
+    colors = [1,2,3]; # 1 is red, 2 is green, 3 is purple
+    shapes = [1,2,3]; # 1 is diamond, 2 is squiggle, 3 is oval
+    shades = [1,2,3]; # 1 is solid, 2 is striped, 3 is  empty
     cards = Array.new(81);
     for number in numbers
       for color in colors
         for shape in shapes
           for shade in shades
-            card = Cards.initialize(number,color,shape,shade);
+            card = Cards.initialize(number,color,shape,shade,false);
             cards.add card;
           end
         end
@@ -68,7 +71,6 @@ class Cards
     i = 0;
     cardShow = Array.new()
     0.upto 12 do
-
       card = cards[rand(cards.length)];
       cards.delete(card);
       cardShow.add(card);
@@ -79,13 +81,13 @@ class Cards
 
   #Add 3 cards to the array of cards that will be shown to the player. And remove them from the original card array.
   def addCards(cards,cardShow)
-    i = 0;
-    while i < 3
-      card = cards[rand(cards.length)];
-      cards.delete(card);
-      cardShow.add(card);
-      i = i + 1;
-    end
+   0.upto 3 do
+     card = cards[rand(cards.length)];
+     cards.delete(card);
+     cardShow.add(card);
+     i = i + 1;
+     
+   end
   end
 
 
@@ -117,8 +119,8 @@ class Cards
     end
     return set
   end
-  
-    #Determine if the card array shown to the user contains a set.
+
+  #Determine if the card array shown to the user contains a set.
   def containSet(cardShow)
     for cardA in cardShow
       for cardB in cardShow
@@ -129,42 +131,41 @@ class Cards
             end
           end
         end
+      end
     end
-  end
 
-  # If Set on the board: highlights the next card in the Set. If no Set on board: adds three new cards
-def get_hint (cardA, cardB, cardShow,cards)
-  i = 0
-  while i<cardShow.length
-    cardC = cardShow[i]
-    cardChosen = [cardA,cardB,cardC]
-    if isSet(cardChosen)
+    # If Set on the board: highlights the next card in the Set. If no Set on board: adds three new cards
+    def get_hint (cardA, cardB, cardShow,cards)
+      0.upto cardShow.length do
+        cardC = cardShow[i]
+        cardChosen = [cardA,cardB,cardC]
+        if isSet(cardChosen)
+          @builder['hint'].visible = false
+          return cardC
+        end
+        i += 1
+      end
+      addCards(cards,cardShow)
       @builder['hint'].visible = false
-      return cardC
     end
-    i += 1
-  end
-  addCards(cards,cardShow)
-  @builder['hint'].visible = false
-end
 
-def gameOver
-  if gameStart == "Game Over"
-    @builder['score'].label = "Score: " + @score.to_s
-  end
-end
+    def gameOver
+      if gameStart == "Game Over"
+        @builder['score'].label = "Score: " + @score.to_s
+      end
+    end
 
-def instruction_button(*args)
-  @builder['instruction'].label = "A SET is three cards where each feature, when looked at individually, is either all
+    def instruction_button(*args)
+      @builder['instruction'].label = "A SET is three cards where each feature, when looked at individually, is either all
                                   the same OR all different. Each card contains four features: color (red, purple or
                                   green), shape (oval, squiggle or diamond), number (one, two or three) and
                                   shading (solid, striped or outlined). SET isa speed game."
-end
+    end
 
-def pause_button(*args)
-  pauseTime = $Timer.Time.now
-  @builder['pause'].label = "Game is stopped, time is : "+ pauseTime.to_s
-  @builder['cards'].visible = false
-end
+    def pause_button(*args)
+      pauseTime = $Timer.Time.now
+      @builder['pause'].label = "Game is stopped, time is : "+ pauseTime.to_s
+      @builder['cards'].visible = false
+    end
 
-end
+  end
