8b3e417feb6e685818597e4360e4763989f357a4
Jian Gao <1304637796@qq.com>
2019-02-03 03:51:34 -0500

    Update deck.rb

--- Project2/deck.rb
+++ Project2/deck.rb
@@ -1,56 +1,48 @@
 class Deck
-
-  def initialize
-    # Get 81 cards and put them in the array. Each of them has different number, color, shape and shade.
-    numbers = [1,2,3];
-    colors = [1,2,3]; # 1 is red, 2 is green, 3 is purple
-    shapes = [1,2,3]; # 1 is diamond, 2 is squiggle, 3 is oval
-    shades = [1,2,3]; # 1 is solid, 2 is striped, 3 is  empty
-    cards = Array.new(81);
-    for number in numbers
-      for color in colors
-        for shape in shapes
-          for shade in shades
-            card = Cards.initialize(number,color,shape,shade,false);
-            cards.add card;
-          end
-        end
-      end
+  @@deck = Array.new
+  @@cardShow = Array.new
+  def newDeck
+    # Get the image wof 81 cards and put them in the array. Each of them has different number, color, shape and shade.
+    i = 0
+    while i<81 do
+      @@deck[i] = "D:/img/" + i.to_s + ".png"
+      i = i + 1
     end
-    @deck = cards
+
   end
 
   #Get random 12 cards and put them in the array. Then remove them from the original array.
   def getRandomCards
-    @cardShow = Array.new()
-    0.upto 12 do
-      card = @deck[rand(@deck.length)];
-      @deck.delete(card);
-      @cardShow.add(card);
+    @@cardShow = Array.new
+    0.upto 11 do
+      card = @@deck[rand(@@deck.length)];
+      @@deck.delete(card);
+      @@cardShow.push(card);
     end
+    return @@cardShow
   end
 
   #Add 3 cards to the array of cards that will be shown to the player. And remove them from the original card array.
   def addCards
     0.upto 3 do
-      card = @deck[rand(@deck.length)];
-      @deck.delete(card);
-      @cardShow.add(card);
+      card = @@deck[rand(@@deck.length)];
+      @@deck.delete(card);
+      @@cardShow.add(card);
     end
   end
 
   #Remove card from the original array of cards
   def removeCard(cardsFormASet)
     0.upto cardsFormASet.length do
-      @deck.delete(cardsFormASet[i]);
+      @@deck.delete(cardsFormASet[i]);
     end
   end
 
   #Determine if the card array shown to the user contains a set.
   def containSet
-    for cardA in @cardShow
-      for cardB in @cardShow
-        for cardC in @cardShow
+    for cardA in @@cardShow
+      for cardB in @@cardShow
+        for cardC in @@cardShow
           if cardA != cardB && cardB !=cardC && cardA!=cardC
             if isSet(cardChosen[cardA,cardB,cardC])
               return true
