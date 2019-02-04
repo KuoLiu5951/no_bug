2e90e6e3b58acedfa9a3bc1f1eee1acf77630a5f
Jian Gao <1304637796@qq.com>
2019-02-04 06:04:50 -0500

    Update deck.rb

--- Project2/deck.rb
+++ Project2/deck.rb
@@ -2,7 +2,7 @@ require_relative 'cards'
 class Deck
   @@deck = Hash.new
   @@cardShow = Hash.new
-  def newDeck
+  def initialize
     # Get the image wof 81 cards and put them in the array. Each of them has different number, color, shape and shade.
     i = 0
     while i<81 do
@@ -12,7 +12,7 @@ class Deck
             for shade in 1..3
               state = false
               card = Cards.new(number,color,shape,shade,state)
-              @@deck[card] = "D:/img/" + number.to_s + "_" + color.to_s + "_" + shape.to_s + "_" + shade.to_s + ".png"
+              @@deck[card] = "D:/img/" + number.to_s + "_" + color.to_s + "_" + shape.to_s + "_" + shade.to_s
             end
           end
         end
@@ -22,6 +22,10 @@ class Deck
     end
   end
 
+  #return the deck hash
+  def getDeck
+    return @@deck
+  end
   #Get random 12 cards and put them in the array. Then remove them from the original array.
   def getRandomCards
     0.upto 11 do
@@ -65,20 +69,5 @@ class Deck
     end
   end
 
-
-  # If Set on the board: highlights the next card in the Set. If no Set on board: adds three new cards
-  def get_hint (cardA, cardB)
-    i = 0
-    0.upto @@cardShow.length do
-      keys = @@cardShow.keys
-      cardC = keys[i]
-      cardChosen = [cardA,cardB,cardC]
-      if isSet(cardChosen)
-        return cardC
-      end
-      i+= 1
-    end
-    addCards
-  end
 
 end
