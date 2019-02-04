8a0080a2959f72f2807da8720ef72f0c8e2d9bc8
Jian Gao <1304637796@qq.com>
2019-02-03 19:41:52 -0500

    Update score.rb

--- Project2/score.rb
+++ Project2/score.rb
@@ -1,11 +1,10 @@
 class Score
-  
+  @@score = 0
   #If player find a set, then get one point
   def Add_score(cardChosen)
     if isSet(cardChosen)
-    @score += 1
+    @@score += 1
     end
-    return @score
   end
   
 end
