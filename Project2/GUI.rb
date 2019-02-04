3602bc4652595e17bbb980e63d5d634d0eb412e2
Jian Gao <1304637796@qq.com>
2019-02-04 08:27:48 -0500

    Update GUI.rb

--- Project2/GUI.rb
+++ Project2/GUI.rb
@@ -46,11 +46,11 @@ Shoes.app(title: "Set Game", width: 600, height: 400) do
         cards = cardShow.keys
         card= cards[0]
         address = cardShow[card]
-        next_image = image  address + ".png", width: 270, height:180, margin: 12
-        next_image.click do
+        image1 = image  address + ".png", width: 270, height:180, margin: 12
+        image1.click do
           if !card.getState
 
-            next_image.path = address + "_c.png"
+            image1.path = address + "_c.png"
             if cardChosen.length <3
               cardChosen.push(card )
               card.switch
@@ -59,14 +59,294 @@ Shoes.app(title: "Set Game", width: 600, height: 400) do
             end
           else
             card.switch
-            next_image.path =address + "_d.png"
+            image1.path =address + "_d.png"
             cardChosen.delete(card)
 
           end
 
         end
+      end
+      flow width:0.25, height:0.2 do
 
+        cards = cardShow.keys
+        card= cards[1]
+        address = cardShow[card]
+        image2 = image  address + ".png", width: 270, height:180, margin: 12
+        image2.click do
+          if !card.getState
 
+            image2.path = address + "_c.png"
+            if cardChosen.length <3
+              cardChosen.push(card )
+              card.switch
+            else
+              alert "You have already chosen 3 cards!"
+            end
+          else
+            card.switch
+            image2.path =address + "_d.png"
+            cardChosen.delete(card)
+
+          end
+
+        end
+      end
+
+      flow width:0.25, height:0.2 do
+
+        cards = cardShow.keys
+        card= cards[2]
+        address = cardShow[card]
+        image3 = image  address + ".png", width: 270, height:180, margin: 12
+        image3.click do
+          if !card.getState
+
+            image2.path = address + "_c.png"
+            if cardChosen.length <3
+              cardChosen.push(card )
+              card.switch
+            else
+              alert "You have already chosen 3 cards!"
+            end
+          else
+            card.switch
+            image3.path =address + "_d.png"
+            cardChosen.delete(card)
+
+          end
+
+        end
+      end
+
+      flow width:0.25, height:0.2 do
+
+        cards = cardShow.keys
+        card= cards[3]
+        address = cardShow[card]
+        image4 = image  address + ".png", width: 270, height:180, margin: 12
+        image4.click do
+          if !card.getState
+
+            image4.path = address + "_c.png"
+            if cardChosen.length <3
+              cardChosen.push(card )
+              card.switch
+            else
+              alert "You have already chosen 3 cards!"
+            end
+          else
+            card.switch
+            image4.path =address + "_d.png"
+            cardChosen.delete(card)
+
+          end
+
+        end
+      end
+
+      flow width:0.25, height:0.2 do
+
+        cards = cardShow.keys
+        card= cards[4]
+        address = cardShow[card]
+        image5 = image  address + ".png", width: 270, height:180, margin: 12
+        image5.click do
+          if !card.getState
+
+            image5.path = address + "_c.png"
+            if cardChosen.length <3
+              cardChosen.push(card )
+              card.switch
+            else
+              alert "You have already chosen 3 cards!"
+            end
+          else
+            card.switch
+            image5.path =address + "_d.png"
+            cardChosen.delete(card)
+
+          end
+
+        end
+      end
+      flow width:0.25, height:0.2 do
+
+        cards = cardShow.keys
+        card= cards[5]
+        address = cardShow[card]
+        image6 = image  address + ".png", width: 270, height:180, margin: 12
+        image6.click do
+          if !card.getState
+
+            image6.path = address + "_c.png"
+            if cardChosen.length <3
+              cardChosen.push(card )
+              card.switch
+            else
+              alert "You have already chosen 3 cards!"
+            end
+          else
+            card.switch
+            image6.path =address + "_d.png"
+            cardChosen.delete(card)
+
+          end
+
+        end
+      end
+      flow width:0.25, height:0.2 do
+
+        cards = cardShow.keys
+        card= cards[6]
+        address = cardShow[card]
+        image7= image  address + ".png", width: 270, height:180, margin: 12
+        image7.click do
+          if !card.getState
+
+            image7.path = address + "_c.png"
+            if cardChosen.length <3
+              cardChosen.push(card )
+              card.switch
+            else
+              alert "You have already chosen 3 cards!"
+            end
+          else
+            card.switch
+            image7.path =address + "_d.png"
+            cardChosen.delete(card)
+
+          end
+
+        end
+      end
+
+      flow width:0.25, height:0.2 do
+
+        cards = cardShow.keys
+        card= cards[7]
+        address = cardShow[card]
+        image8 = image  address + ".png", width: 270, height:180, margin: 12
+        image8.click do
+          if !card.getState
+
+            image8.path = address + "_c.png"
+            if cardChosen.length <3
+              cardChosen.push(card )
+              card.switch
+            else
+              alert "You have already chosen 3 cards!"
+            end
+          else
+            card.switch
+            image8.path =address + "_d.png"
+            cardChosen.delete(card)
+
+          end
+
+        end
+      end
+
+      flow width:0.25, height:0.2 do
+
+        cards = cardShow.keys
+        card= cards[8]
+        address = cardShow[card]
+        image9 = image  address + ".png", width: 270, height:180, margin: 12
+        image9.click do
+          if !card.getState
+
+            image9.path = address + "_c.png"
+            if cardChosen.length <3
+              cardChosen.push(card )
+              card.switch
+            else
+              alert "You have already chosen 3 cards!"
+            end
+          else
+            card.switch
+            image9.path =address + "_d.png"
+            cardChosen.delete(card)
+
+          end
+
+        end
+      end
+
+      flow width:0.25, height:0.2 do
+
+        cards = cardShow.keys
+        card= cards[9]
+        address = cardShow[card]
+        image10 = image  address + ".png", width: 270, height:180, margin: 12
+        image10.click do
+          if !card.getState
+
+            image10.path = address + "_c.png"
+            if cardChosen.length <3
+              cardChosen.push(card )
+              card.switch
+            else
+              alert "You have already chosen 3 cards!"
+            end
+          else
+            card.switch
+            image10.path =address + "_d.png"
+            cardChosen.delete(card)
+
+          end
+
+        end
+      end
+
+      flow width:0.25, height:0.2 do
+
+        cards = cardShow.keys
+        card= cards[10]
+        address = cardShow[card]
+        image11 = image  address + ".png", width: 270, height:180, margin: 12
+        image11.click do
+          if !card.getState
+
+            image11.path = address + "_c.png"
+            if cardChosen.length <3
+              cardChosen.push(card )
+              card.switch
+            else
+              alert "You have already chosen 3 cards!"
+            end
+          else
+            card.switch
+            image11.path =address + "_d.png"
+            cardChosen.delete(card)
+
+          end
+
+        end
+      end
+
+      flow width:0.25, height:0.2 do
+
+        cards = cardShow.keys
+        card= cards[11]
+        address = cardShow[card]
+        image12 = image  address + ".png", width: 270, height:180, margin: 12
+        image12.click do
+          if !card.getState
+
+            image12.path = address + "_c.png"
+            if cardChosen.length <3
+              cardChosen.push(card )
+              card.switch
+            else
+              alert "You have already chosen 3 cards!"
+            end
+          else
+            card.switch
+            image12.path =address + "_d.png"
+            cardChosen.delete(card)
+          end
+
+        end
       end
     end
 
