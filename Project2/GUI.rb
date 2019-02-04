6faa02552ac71cdd1d597577a6faa6fd36d7c32b
Jian Gao <1304637796@qq.com>
2019-02-04 09:30:18 -0500

    Update GUI.rb

--- Project2/GUI.rb
+++ Project2/GUI.rb
@@ -1,7 +1,15 @@
 require_relative 'deck'
 require_relative 'cards'
 
+
+
 Shoes.app(title: "Set Game", width: 600, height: 400) do
+  deckClass = Deck.new
+  @@deck = deckClass.getDeck
+  @@cardShow = deckClass.getRandomCards
+  $cardChosen = Array.new
+
+
 
   flow width:1080, height:1125 do
     flow width:1.0, height: 0.1 do
@@ -11,13 +19,26 @@ Shoes.app(title: "Set Game", width: 600, height: 400) do
 
       end
       button 'restart',width:150,height:60 do
-
-
+        deckClass = Deck.new
+        @@deck = deckClass.getDeck
+        @@cardShow = deckClass.getRandomCards
+        $cardChosen = Array.new
       end
+
+
+
+
       button 'Add 3 Cards',width:150,height:60 do
-
-
+        if deckClass.containSet?
+          alert "There is a set among these cards!"
+        else
+          deckClass.addCards
+        end
       end
+
+
+
+
       button 'Hint',width:150,height:60 do
 
 
@@ -36,56 +57,55 @@ Shoes.app(title: "Set Game", width: 600, height: 400) do
       end
     end
     flow  width:1.0, height: 0.8 do
-      deckClass = Deck.new
-      deck = deckClass.getDeck
-      cardShow = deckClass.getRandomCards
 
-      cardChosen = Array.new
       flow width:0.25, height:0.2 do
 
-        cards = cardShow.keys
+        cards = @@cardShow.keys
         card= cards[0]
-        address = cardShow[card]
+        address = @@cardShow[card]
         image1 = image  address + ".png", width: 270, height:180, margin: 12
         image1.click do
           if !card.getState
 
             image1.path = address + "_c.png"
-            if cardChosen.length <3
-              cardChosen.push(card )
+            if $cardChosen.length <3
+              $cardChosen.push(card )
               card.switch
             else
+              image1.path =address + "_d.png"
               alert "You have already chosen 3 cards!"
             end
           else
             card.switch
             image1.path =address + "_d.png"
-            cardChosen.delete(card)
+            $cardChosen.delete(card)
 
           end
 
         end
       end
+
       flow width:0.25, height:0.2 do
 
-        cards = cardShow.keys
+        cards = @@cardShow.keys
         card= cards[1]
-        address = cardShow[card]
-        image2 = image  address + ".png", width: 270, height:180, margin: 12
+        address = @@cardShow[card]
+        image2= image  address + ".png", width: 270, height:180, margin: 12
         image2.click do
           if !card.getState
 
             image2.path = address + "_c.png"
-            if cardChosen.length <3
-              cardChosen.push(card )
+            if $cardChosen.length <3
+              $cardChosen.push(card )
               card.switch
             else
+              image2.path =address + "_d.png"
               alert "You have already chosen 3 cards!"
             end
           else
             card.switch
             image2.path =address + "_d.png"
-            cardChosen.delete(card)
+            $cardChosen.delete(card)
 
           end
 
@@ -94,24 +114,25 @@ Shoes.app(title: "Set Game", width: 600, height: 400) do
 
       flow width:0.25, height:0.2 do
 
-        cards = cardShow.keys
+        cards = @@cardShow.keys
         card= cards[2]
-        address = cardShow[card]
+        address = @@cardShow[card]
         image3 = image  address + ".png", width: 270, height:180, margin: 12
         image3.click do
           if !card.getState
 
-            image2.path = address + "_c.png"
-            if cardChosen.length <3
-              cardChosen.push(card )
+            image3.path = address + "_c.png"
+            if $cardChosen.length <3
+              $cardChosen.push(card )
               card.switch
             else
+              image3.path =address + "_d.png"
               alert "You have already chosen 3 cards!"
             end
           else
             card.switch
             image3.path =address + "_d.png"
-            cardChosen.delete(card)
+            $cardChosen.delete(card)
 
           end
 
@@ -120,24 +141,25 @@ Shoes.app(title: "Set Game", width: 600, height: 400) do
 
       flow width:0.25, height:0.2 do
 
-        cards = cardShow.keys
+        cards = @@cardShow.keys
         card= cards[3]
-        address = cardShow[card]
+        address = @@cardShow[card]
         image4 = image  address + ".png", width: 270, height:180, margin: 12
         image4.click do
           if !card.getState
 
             image4.path = address + "_c.png"
-            if cardChosen.length <3
-              cardChosen.push(card )
+            if $cardChosen.length <3
+              $cardChosen.push(card )
               card.switch
             else
+              image4.path =address + "_d.png"
               alert "You have already chosen 3 cards!"
             end
           else
             card.switch
             image4.path =address + "_d.png"
-            cardChosen.delete(card)
+            $cardChosen.delete(card)
 
           end
 
@@ -146,74 +168,79 @@ Shoes.app(title: "Set Game", width: 600, height: 400) do
 
       flow width:0.25, height:0.2 do
 
-        cards = cardShow.keys
+        cards = @@cardShow.keys
         card= cards[4]
-        address = cardShow[card]
+        address = @@cardShow[card]
         image5 = image  address + ".png", width: 270, height:180, margin: 12
         image5.click do
           if !card.getState
 
             image5.path = address + "_c.png"
-            if cardChosen.length <3
-              cardChosen.push(card )
+            if $cardChosen.length <3
+              $cardChosen.push(card )
               card.switch
             else
+              image5.path =address + "_d.png"
               alert "You have already chosen 3 cards!"
             end
           else
             card.switch
             image5.path =address + "_d.png"
-            cardChosen.delete(card)
+            $cardChosen.delete(card)
 
           end
 
         end
       end
+
       flow width:0.25, height:0.2 do
 
-        cards = cardShow.keys
+        cards = @@cardShow.keys
         card= cards[5]
-        address = cardShow[card]
+        address = @@cardShow[card]
         image6 = image  address + ".png", width: 270, height:180, margin: 12
         image6.click do
           if !card.getState
 
             image6.path = address + "_c.png"
-            if cardChosen.length <3
-              cardChosen.push(card )
+            if $cardChosen.length <3
+              $cardChosen.push(card )
               card.switch
             else
+              image6.path =address + "_d.png"
               alert "You have already chosen 3 cards!"
             end
           else
             card.switch
             image6.path =address + "_d.png"
-            cardChosen.delete(card)
+            $cardChosen.delete(card)
 
           end
 
         end
       end
+
       flow width:0.25, height:0.2 do
 
-        cards = cardShow.keys
+        cards = @@cardShow.keys
         card= cards[6]
-        address = cardShow[card]
-        image7= image  address + ".png", width: 270, height:180, margin: 12
+        address = @@cardShow[card]
+        image7 = image  address + ".png", width: 270, height:180, margin: 12
         image7.click do
           if !card.getState
 
             image7.path = address + "_c.png"
-            if cardChosen.length <3
-              cardChosen.push(card )
+            if $cardChosen.length <3
+              $cardChosen.push(card )
               card.switch
             else
+              image7.path =address + "_d.png"
               alert "You have already chosen 3 cards!"
             end
           else
             card.switch
             image7.path =address + "_d.png"
-            cardChosen.delete(card)
+            $cardChosen.delete(card)
 
           end
 
@@ -222,24 +249,25 @@ Shoes.app(title: "Set Game", width: 600, height: 400) do
 
       flow width:0.25, height:0.2 do
 
-        cards = cardShow.keys
+        cards = @@cardShow.keys
         card= cards[7]
-        address = cardShow[card]
+        address = @@cardShow[card]
         image8 = image  address + ".png", width: 270, height:180, margin: 12
         image8.click do
           if !card.getState
 
             image8.path = address + "_c.png"
-            if cardChosen.length <3
-              cardChosen.push(card )
+            if $cardChosen.length <3
+              $cardChosen.push(card )
               card.switch
             else
+              image8.path =address + "_d.png"
               alert "You have already chosen 3 cards!"
             end
           else
             card.switch
             image8.path =address + "_d.png"
-            cardChosen.delete(card)
+            $cardChosen.delete(card)
 
           end
 
@@ -248,24 +276,25 @@ Shoes.app(title: "Set Game", width: 600, height: 400) do
 
       flow width:0.25, height:0.2 do
 
-        cards = cardShow.keys
+        cards = @@cardShow.keys
         card= cards[8]
-        address = cardShow[card]
+        address = @@cardShow[card]
         image9 = image  address + ".png", width: 270, height:180, margin: 12
         image9.click do
           if !card.getState
 
             image9.path = address + "_c.png"
-            if cardChosen.length <3
-              cardChosen.push(card )
+            if $cardChosen.length <3
+              $cardChosen.push(card )
               card.switch
             else
+              image9.path =address + "_d.png"
               alert "You have already chosen 3 cards!"
             end
           else
             card.switch
             image9.path =address + "_d.png"
-            cardChosen.delete(card)
+            $cardChosen.delete(card)
 
           end
 
@@ -274,24 +303,25 @@ Shoes.app(title: "Set Game", width: 600, height: 400) do
 
       flow width:0.25, height:0.2 do
 
-        cards = cardShow.keys
+        cards = @@cardShow.keys
         card= cards[9]
-        address = cardShow[card]
+        address = @@cardShow[card]
         image10 = image  address + ".png", width: 270, height:180, margin: 12
         image10.click do
           if !card.getState
 
             image10.path = address + "_c.png"
-            if cardChosen.length <3
-              cardChosen.push(card )
+            if $cardChosen.length <3
+              $cardChosen.push(card )
               card.switch
             else
+              image10.path =address + "_d.png"
               alert "You have already chosen 3 cards!"
             end
           else
             card.switch
             image10.path =address + "_d.png"
-            cardChosen.delete(card)
+            $cardChosen.delete(card)
 
           end
 
@@ -300,24 +330,25 @@ Shoes.app(title: "Set Game", width: 600, height: 400) do
 
       flow width:0.25, height:0.2 do
 
-        cards = cardShow.keys
+        cards = @@cardShow.keys
         card= cards[10]
-        address = cardShow[card]
+        address = @@cardShow[card]
         image11 = image  address + ".png", width: 270, height:180, margin: 12
         image11.click do
           if !card.getState
 
             image11.path = address + "_c.png"
-            if cardChosen.length <3
-              cardChosen.push(card )
+            if $cardChosen.length <3
+              $cardChosen.push(card )
               card.switch
             else
+              image11.path =address + "_d.png"
               alert "You have already chosen 3 cards!"
             end
           else
             card.switch
             image11.path =address + "_d.png"
-            cardChosen.delete(card)
+            $cardChosen.delete(card)
 
           end
 
@@ -326,29 +357,34 @@ Shoes.app(title: "Set Game", width: 600, height: 400) do
 
       flow width:0.25, height:0.2 do
 
-        cards = cardShow.keys
+        cards = @@cardShow.keys
         card= cards[11]
-        address = cardShow[card]
+        address = @@cardShow[card]
         image12 = image  address + ".png", width: 270, height:180, margin: 12
         image12.click do
           if !card.getState
 
             image12.path = address + "_c.png"
-            if cardChosen.length <3
-              cardChosen.push(card )
+            if $cardChosen.length <3
+              $cardChosen.push(card )
               card.switch
             else
+              image12.path =address + "_d.png"
               alert "You have already chosen 3 cards!"
             end
           else
             card.switch
             image12.path =address + "_d.png"
-            cardChosen.delete(card)
+            $cardChosen.delete(card)
           end
 
         end
       end
+
+
     end
+
+
 
 
 
