fa669e0300a10796e11cf1d59ba243c94b856292
KuoLiu5951 <46659953+KuoLiu5951@users.noreply.github.com>
2019-02-05 00:47:50 -0500

    Update timer.rb

--- Project2/timer.rb
+++ Project2/timer.rb
@@ -1,14 +1,11 @@
 Shoes.app do
+  $mode = "Ey"
   flow do
     button 'Timer', width: 100, height: 50 do
-      window width: 200, height: 130 do
-        @second = 0
-        if @mode = "easy"
-          @seconds = 120
+    if $mode == "Ey"
+      flow width: 200, height: 130 do
 
-        elsif @mode = "hard"
-          @seconds = 60
-        end
+          seconds = 120
         tenths = 0
         clock =  '%02d:%02d'
 
@@ -44,6 +41,49 @@ Shoes.app do
         end
 
       end
+    else
+      flow width: 200, height: 130 do
+
+            seconds = 60
+        tenths = 0
+        clock =  '%02d:%02d'
+
+        flow do
+          @show = para clock % [seconds, tenths], size: 50, stroke: black
+        end
+
+        a = animate(10) do
+          tenths -= 1
+
+          if tenths < 0
+            tenths  = 9
+            seconds -= 1
+          end
+
+          @show.text = clock % [seconds, tenths]
+
+          if seconds == 0 and tenths == 0
+            a.stop
+            alert("game over")
+            close
+          end
+
+        end
+        @stop = button "stop"
+        @stop.click do
+          a.stop
+          alert("Pause")
+        end
+        @start = button "start"
+        @start.click do
+          a.start
+        end
+
+      end
+
+    end
+
     end
   end
-end
+  end
+
