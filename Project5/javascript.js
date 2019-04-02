86a54934a4e80c0d269e67272f5e8e5ed496015c
JackyGaoJ <1304637796@qq.com>
2019-04-02 00:59:24 -0400

    Upload the final project. And combine the time.js with the javascript.js

--- Project5/javascript.js
+++ Project5/javascript.js
@@ -10,6 +10,7 @@ function Card(shade, shape, color, number,path){
     this.path = path;
 }
 
+var emptyCard = new Card(0,0,0,"images/emptyCard.png");
 /*
     Create the deck contains 81 cards.Shade = 1,2,3 means solid, striped, open; Shape = 1,2,3 means squiggle, diamond, oval; color = 1,2,3 means red, purple, green; number = 1,2,3 means the number of shapes.
 */
@@ -20,7 +21,7 @@ function createDeck(deck){
             for(color = 1; color < 4; color ++){
                 for(number = 1; number <4; number ++){
                     path = pathConst + shade + "_" + shape + "_" + color + "_" + number + ".png";
-                    card = new Card(number, color ,shape, shade, path);
+                    card = new Card(shade, shape ,color, number, path);
                     deck.push(card);
                 }
             }
@@ -63,6 +64,7 @@ function addThreeCards(cardShow,deck){
         for (j = 0; j<3; j++){
             cardTable[cardShowLength+j].firstElementChild.src=cardShow[cardShowLength+j].path;
         }
+        document.getElementById("cardsRemain").innerHTML  = document.getElementById("cardsRemain").innerHTML- 3;
 
     }
     return cardShow;
@@ -89,10 +91,7 @@ function isSet(cardSelected){
     return set;
 }
 
-/*
-    Give a hint to the player according to the two cards he/she has already choosen.
-*/
-function hint(cardSelected){
+function hintClick(){
     if (cardSelected.length < 2){
         alert("You must choose 2 cards before get a hint!")
     }
@@ -100,29 +99,36 @@ function hint(cardSelected){
         card1 = cardSelected[0];
         card2 = cardSelected[1];
         let aSet = false;
-        
-        for(i=0;i<cardShow.length-1 && !aSet;i++){
-            cardSelected.push(cardShow[i]);
+        for(i=0;i<cardShow.length && !aSet;i++){
+            cardHint = cardShow[i];
+            cardSelected.push(cardHint);
             if (isSet(cardSelected)){
-                /* Show a window gives the user that card*/ 
-                cardSelected.pop();
+                let cardShade = ["solid","striped","open"];
+                let cardShape = ["squiggle", "diamond", "oval"]; 
+                let cardColor= ["red", "purple", "green"];
+                alert("The Card that will form a set is "+cardColor[cardHint.color-1]+" ," + cardShade[cardHint.shade-1] + " ," + cardShape[cardHint.shape-1]+ " contains " + cardHint.number + " shapes!" );
+               
                 aSet = true;
             }
+            cardSelected.pop();
         }
         if (!aSet){
             alert("You've chosen wrong cards! Try again!");
+         
         }
     }
 }
-function submitClick(){
-    alert("Information saved successfully!")
-    window.location.replace("cards.html")
-}
+
+
 
 function restartClick(){
     deck = [];
     cardShow = [];
     cardSelected = [];
+    document.getElementById("score").innerHTML=0;
+    cdreset();
+    countdown();
+    
     createDeck(deck);
     cardShow = getRandom(12,deck);
     for(i=0;i<cardShow.length;i++){
@@ -131,19 +137,29 @@ function restartClick(){
         let img = document.getElementById(id);
         img.src = cardShow[i].path;
     }
+    let cardsTable = document.getElementsByClassName("cards");
+    for(x = cardShow.length; x<cardsTable.length;x++){
+        cardsTable[x].firstElementChild.src = "images/empty.png";
+    }
+    document.getElementById("cardsRemain").innerHTML = 81 - cardShow.length;
 }
 
 let cardsTable = document.getElementsByClassName("cards");
-for (i = 0; i<cardsTable.length-1;i++){
+for (i = 0; i<cardsTable.length;i++){
     cardsTable[i].onclick = function cardClick(){
-        
-        let id = this.firstElementChild.id;
+       
+        if (this.firstElementChild.src=="images/empty.png"){
+            alert("You cannot select an empty card!")
+        }
+        else{
+            let id = this.firstElementChild.id;
         let index = id.replace('card','') -1;
         if (index > cardShow.length-1){
             alert("You cannot select an empty card!");
         }
         else{
             card = cardShow[index];
+           
             if (cardSelected.length<3){
                 let pathString = card.path;
                 if (pathString.indexOf('c') > -1){
@@ -161,19 +177,39 @@ for (i = 0; i<cardsTable.length-1;i++){
                         let set = isSet(cardSelected);
                         if (set){
                             alert("Congratulations! This is a set!");
-                            /*
-                                Add Score
-                            */
-                            for(i=0;i<3;i++){
-                                selectedCard = cardSelected[i];
-                                
-                                let cardIndex = cardShow.indexOf(selectedCard);
-                                let random = Math.floor(Math.random() * deck.length);
-                                newCard = deck[random];
-                                cardShow[cardIndex]=newCard;
-                                cardsTable[cardIndex].firstElementChild.src = newCard.path;
-                                deck.splice(random,1);
+                            document.getElementById("score").innerHTML++;
+
+                            
+                            console.log(score);
+                            if (deck.length>=3){
+                                for(i=0;i<3;i++){
+                                    selectedCard = cardSelected[i];
+                                    let cardIndex = cardShow.indexOf(selectedCard);
+                                    let random = Math.floor(Math.random() * deck.length);
+                                    newCard = deck[random];
+                                    cardShow[cardIndex]=newCard;
+                                    cardsTable[cardIndex].firstElementChild.src = newCard.path;
+                                    deck.splice(random,1);
+                                }
                             }
+                            else{
+                                for(i=0;i<3;i++){
+                                    selectedCard = cardSelected[i];
+                                    let cardIndex = cardShow.indexOf(selectedCard);
+                                    cardShow[cardIndex]=emptyCard;
+                                    cardsTable[cardIndex].firstElementChild.src = emptyCard.path;
+                                }
+
+                            }
+                           
+                            let cardsInHtml = document.getElementById("cardsRemain").innerHTML;
+                            if (cardsInHtml>=3){
+                                document.getElementById("cardsRemain").innerHTML= cardsInHtml- 3;
+                            }
+                            else{
+                                alert("Congratulations! You chose all sets! Your total score is 27. Please click restart button if you want to try again.")
+                            }
+                             
                             cardSelected = [];
                         }
                         else{
@@ -190,12 +226,48 @@ for (i = 0; i<cardsTable.length-1;i++){
             
            
         }
+
+        }
+        
         
     }
 }
 
 
+/*
+    Following is the time part.
+*/
+var CCOUNT = 1800;
 
+var t, count;
+
+function cddisplay() {
+    // displays time in span
+    document.getElementById('timespan').innerHTML = count;
+};
+
+function countdown() {
+    // starts countdown
+    cddisplay();
+    if (count == 0) {
+        alert("Game over! You score is " + document.getElementById("score").innerHTML + "! Please click restart button if you want to play again!");
+    } else {
+        count--;
+        t = setTimeout("countdown()", 1000);
+    }
+};
+
+function cdpause() {
+    // pauses countdown
+    clearTimeout(t);
+};
+
+function cdreset() {
+    // resets countdown
+    cdpause();
+    count = CCOUNT;
+    cddisplay();
+};
 
     
 
