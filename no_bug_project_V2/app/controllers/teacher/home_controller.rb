b3368d73d3feb0ecb1c4eaa07353b9fe021775c9
Jian Gao <1304637796@qq.com>
2019-04-25 23:07:59 -0400

    Deleted and will upload later

--- no_bug_project_V2/app/controllers/teacher/home_controller.rb
+++
@@ -1,4 +0,0 @@
-class Teacher::HomeController < Teacher::BaseController
-  def index
-  end
-end
--- no_bug_project_V2/db/migrate/20190414034053_create_teacher_and_students.rb
+++
@@ -1,9 +0,0 @@
-class CreateTeacherAndStudents < ActiveRecord::Migration[5.1]
-  def change
-    create_table :teacher_and_students do |t|
-      t.integer :teacher_id, index: true
-      t.integer :student_id, index: true
-      t.timestamps
-    end
-  end
-end
